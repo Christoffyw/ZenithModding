15040e65d4ee4e41d78d015704be58502494c745
Christoffyw <55860414+Christoffyw@users.noreply.github.com>
2022-02-21 10:55:43 -0500

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,4 @@
-powershell -Command "Invoke-WebRequest https://builds.bepinex.dev/projects/bepinex_be/549/BepInEx_UnityMono_x64_f2c0e0f_6.0.0-be.549.zip -OutFile temp_download.zip"
+powershell -Command "Invoke-WebRequest https://builds.bepinex.dev/projects/bepinex_be/549/BepInEx_UnityIL2CPP_x64_f2c0e0f_6.0.0-be.549.zip -OutFile temp_download.zip"
 powershell Expand-Archive temp_download.zip -DestinationPath ./
 del temp_download.zip
 powershell -Command "Invoke-WebRequest https://github.com/Christoffyw/ZenithModding/releases/latest/download/ZenithModding.exe -OutFile ZenithModding.exe"
